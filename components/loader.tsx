bff677738a567a20b73e628dd820964d92e498c9
Lakshya Singh <lakshay.singh1108@gmail.com>
2021-02-26 12:48:53 +0530

    feat: loader component

---
+++ components/loader.tsx
@@ -0,0 +1,72 @@
+import style from "styles/loader.module.css";
+
+export default function Loader() {
+  return (
+    <div className={style.preloader}>
+      <div className={style.preloader__ring}>
+        <div className={style.preloader__sector}>L</div>
+        <div className={style.preloader__sector}>o</div>
+        <div className={style.preloader__sector}>a</div>
+        <div className={style.preloader__sector}>d</div>
+        <div className={style.preloader__sector}>i</div>
+        <div className={style.preloader__sector}>n</div>
+        <div className={style.preloader__sector}>g</div>
+        <div className={style.preloader__sector}>.</div>
+        <div className={style.preloader__sector}>.</div>
+        <div className={style.preloader__sector}>.</div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+      </div>
+      <div className={style.preloader__ring}>
+        <div className={style.preloader__sector}>L</div>
+        <div className={style.preloader__sector}>o</div>
+        <div className={style.preloader__sector}>a</div>
+        <div className={style.preloader__sector}>d</div>
+        <div className={style.preloader__sector}>i</div>
+        <div className={style.preloader__sector}>n</div>
+        <div className={style.preloader__sector}>g</div>
+        <div className={style.preloader__sector}>.</div>
+        <div className={style.preloader__sector}>.</div>
+        <div className={style.preloader__sector}>.</div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+        <div className={style.preloader__sector}></div>
+      </div>
+    </div>
+  );
+}
